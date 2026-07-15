dd5741f21456dbafa378dc19dcfee02cc6f683ff
Chase Hoffman <132788890+chasehof@users.noreply.github.com>
2026-07-15 19:59:39 -0400

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        size_t i{0};
+        size_t j{numbers.size() - 1};
+
+        while(numbers[i] + numbers[j] != target){
+            if(numbers[i] + numbers[j] > target){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+
+        return {static_cast<int>(i+1), static_cast<int>(j + 1)};        
+    }
+};
