2c0f001b2f784061362d48397010bcf267359340
Chase Hoffman <132788890+chasehof@users.noreply.github.com>
2026-07-15 20:33:39 -0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int i{0};
+        int j{1};
+        int max{0};
+
+        while(j < prices.size()){
+            if(prices[j] < prices[i]){
+                i = j;
+                j++;
+            }
+            else{
+                max = std::max(max, prices[j] - prices[i]);
+                j++;
+            }
+        }
+
+        if(max < 0){
+            return 0;
+        }
+
+        return max;
+    }
+};
